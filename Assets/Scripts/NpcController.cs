d63f45799331a1da30c7e496f4ceaca66ccec282
44378 <chen.jiayua@husky.neu.ledu>
2022-11-20 23:43:56 -0500

    source code for tutorial

---
+++ Assets/Scripts/NpcController.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcController : MonoBehaviour
+{    
+    public enum BehaviorState
+    {
+        Idle,
+        Walk,
+        //Dance,
+        RunAway
+    }    
+    BehaviorState currentState;
+
+    public Transform target;
+    public float idleTime = 5f;
+    public float walkTime = 5f;
+    public float chaseDistance = 3f;
+    public float safeDistance = 8f;
+
+    [SerializeField] 
+    private CharacterController controller;
+
+
+    Animator anim;
+    [SerializeField] 
+
+    float distanceToPlayer;
+    float counter;
+    private float gravity = -9.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(target == null) {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        anim = GetComponent<Animator>();
+        currentState = BehaviorState.Walk;
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, target.position);
+        
+        switch(currentState)
+        {
+            case BehaviorState.Idle:
+                Idle();
+                break;
+            case BehaviorState.Walk:
+                Walk();
+                break;
+            // case BehaviorState.Dance:
+            //     Dance();
+            //     break;
+            case BehaviorState.RunAway:
+                RunAway();
+                break;
+        }
+    }
+
+    void Idle() 
+    {
+        anim.SetInteger("animState", 1);
+        counter += Time.deltaTime;
+        if (counter > idleTime)
+        {
+            currentState = BehaviorState.Walk;
+            counter = 0;
+        }
+        if (distanceToPlayer <= chaseDistance)
+        {
+            currentState = BehaviorState.RunAway;
+        }
+    }
+
+    void Walk()
+    {
+        anim.SetInteger("animState", 2);
+
+        // Rotate randomly 15 degrees
+        transform.Rotate(0, Random.Range(-15, 15), 0);
+        controller.Move(transform.forward * 2 * Time.deltaTime);
+
+        counter += Time.deltaTime;
+        if (counter > walkTime) 
+        {
+            currentState = BehaviorState.Idle;
+            counter = 0;
+        }
+
+        if (distanceToPlayer <= chaseDistance)
+        {
+            currentState = BehaviorState.RunAway;
+        }
+
+    }
+
+    void Dance()
+    {
+        // anim.SetInteger("animState", 3);
+        // if (distanceToPlayer <= chaseDistance)
+        // {
+        //     currentState = BehaviorState.RunAway;
+        // }
+    }
+
+    void RunAway()
+    {
+        anim.SetInteger("animState", 4);
+        
+        // rotate to face away from player
+        Vector3 direction = transform.position - target.position;
+        direction.y = 0;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+        controller.Move(transform.forward * 5 * Time.deltaTime);
+
+        if (distanceToPlayer > safeDistance)
+        {
+            currentState = BehaviorState.Idle;
+        }
+    }
+
+
+
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,22 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Insert character controller")]
     private CharacterController controller;
 
+    [SerializeField] 
+    [Tooltip("Insert camera")]
+    private Camera mainCamera;
+
+    [SerializeField] 
+    [Tooltip("Insert animator controller")]
+    private Animator trainerAnimator;
+
+
     private Vector3 velocity;
     private float gravity = -9.8f;
-    private float jumpHeightWithoutGravity = 2f;
-    public float speed = 5f;
+    private float jumpHeightWithoutGravity = 10f;
     public float maxDistanceToJump;
+
+    public float speed = 5f;
+    public float runSpeed = 10f;
 
     public bool grounded;
 
@@ -27,15 +38,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Grabs the transforms
+        Transform playerTransform = transform;
+        Transform cameraTransform = mainCamera.transform;
+
         //Ground Movement
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Transform playerTransform = transform;
+        
         grounded = Physics.Raycast(playerTransform.position, Vector3.down, maxDistanceToJump);
         
+        //Rotate along the camera
+        playerTransform.rotation = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y,Vector3.up);
 
         Vector3 movement = (playerTransform.right * x) + (playerTransform.forward * z);
-        controller.Move(movement * (speed * Time.deltaTime));        
+
+        //Control player speed
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            controller.Move(movement * (runSpeed * Time.deltaTime));
+        }
+        else
+        {
+            controller.Move(movement * (speed * Time.deltaTime));   
+        }
+             
         //Gravity and Jumping
         if (!grounded)
         {
